0452ce2b18ffe33c69ab480b240c7d0c6ab267dd
V5n <iguru.shou.5410070@gmail.com>
2020-09-07 16:57:20 +0700

    Fix

--- src/pages/DayPage.tsx
+++ src/pages/DayPage.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
-const DayPage = () => <h1>Day Phase</h1>
+import { Gap } from 'solarxui';
+
+const DayPage = () => (
+    <Gap type="vertical" size="8px">
+        <h1>Day Phase</h1>
+    </Gap>
+)
 
 export default DayPage
