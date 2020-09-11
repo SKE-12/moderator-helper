34a12cc57bd0f52275d2bd9034a4102f9bf3be4b
gunphasin <phasin@wongnai.com>
2020-09-11 23:33:17 +0700

    fix end game logic

--- src/pages/InputPage.tsx
+++ src/pages/InputPage.tsx
@@ -1,83 +1,95 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Case from 'case';
-import { Photo } from 'solarxui';
-import styled from 'styled-components';
+import Case from "case";
+import { Photo } from "solarxui";
+import styled from "styled-components";
 
-import { Button } from '@material-ui/core';
+import { Button } from "@material-ui/core";
 
-import { useGameState } from '../contexts/gameController';
-import Player from '../models/Player';
-import roleImgResolver from '../roleImgResolver';
+import { useGameState } from "../contexts/gameController";
+import Player from "../models/Player";
+import roleImgResolver from "../roleImgResolver";
 
 const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 100%;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
 
 const Header = styled.h1`
-    font-size: 32px;
-    font-weight: bold;
-    text-align: center
-`
+  font-size: 32px;
+  font-weight: bold;
+  text-align: center;
+`;
 
 const RoleName = styled.h2`
-    font-size: 24px;
-    font-weight: bold;
-`
+  font-size: 24px;
+  font-weight: bold;
+`;
 
 const InputName = styled.input`
-    font-size: 20px;
-    border: 1px solid black;
-    width: 40%
-    vertical-align: middle;
-    padding 1%;
-    border-radius: 20px;
-    outline: none;
-`
+  font-size: 20px;
+  border: 1px solid black;
+  width: 40%;
+  vertical-align: middle;
+  padding: 1%;
+  border-radius: 20px;
+  outline: none;
+`;
 
 const InputPage = () => {
-    const { activePlayerClasses, setPlayerAndStartGame } = useGameState(gameState => ({
-        activePlayerClasses: gameState.activePlayerClasses,
-        setPlayerAndStartGame: gameState.setPlayerAndStartGame,
-    }))
-    const [activeClasses, setActiveClasses] = useState<any>(activePlayerClasses)
-    const [players, setPlayers] = useState<Player[]>([])
-    const [name, setName] = useState('')
+  const { activePlayerClasses, setPlayerAndStartGame } = useGameState(
+    (gameState) => ({
+      activePlayerClasses: gameState.activePlayerClasses,
+      setPlayerAndStartGame: gameState.setPlayerAndStartGame,
+    })
+  );
+  const [activeClasses, setActiveClasses] = useState<any>(activePlayerClasses);
+  const [players, setPlayers] = useState<Player[]>([]);
+  const [name, setName] = useState("");
 
-    const onChange = ({ target: { value }}: any) => {
-        setName(value)
+  const onChange = ({ target: { value } }: any) => {
+    setName(value);
+  };
+
+  const onSubmit = () => {
+    const [ActiveClass, ...otherClasses] = activeClasses;
+    const player = new ActiveClass() as Player;
+    player.playerName = name;
+
+    if (otherClasses.length === 0) {
+      const fullPlayers = [...players, player].sort((p1, p2) =>
+        p1.order < p2.order ? 1 : -1
+      );
+      setPlayerAndStartGame(fullPlayers);
+    } else {
+      setActiveClasses(otherClasses);
+      setName("");
+      setPlayers([...players, player]);
     }
+  };
 
-    const onSubmit = () => {
-        const [ActiveClass, ...otherClasses] = activeClasses
-        const player = new ActiveClass() as Player
-        player.playerName = name
+  const activeClass = activeClasses[0];
 
-        if (otherClasses.length === 0) {
-            const fullPlayers = [...players, player].sort((p1, p2) => p1.order < p2.order ? 1 : -1)
-            setPlayerAndStartGame(fullPlayers)
-        } else {
-            setActiveClasses(otherClasses)
-            setName('')
-            setPlayers([...players, player])
-        }
-    }
+  return (
+    <Container>
+      <Header>Input Name</Header>
+      <Photo size={400} src={roleImgResolver(Case.snake(activeClass.name))} />
+      <RoleName>{Case.title(activeClass.name)}</RoleName>
+      <div hidden={activeClass.name !== "PlagueDoctor"}>
+        (หากมี Sinister Villager ให้ Sinister Villager ลืมตาขึ้นมาด้วย)
+      </div>
+      <InputName onChange={onChange} value={name} />
+      <Button
+        variant="outlined"
+        style={{ marginTop: "20px" }}
+        onClick={onSubmit}
+      >
+        Submit
+      </Button>
+    </Container>
+  );
+};
 
-    const activeClass = activeClasses[0]
-
-    return (
-        <Container>
-            <Header>Input Name</Header>
-            <Photo size={400} src={roleImgResolver(Case.snake(activeClass.name))}/>
-            <RoleName>{Case.title(activeClass.name)}</RoleName>
-            <div hidden={activeClass.name !== 'PlagueDoctor'}>(หากมี Sinister Villager ให้ Sinister Villager ลืมตาขึ้นมาด้วย)</div>
-            <InputName onChange={onChange} value={name} />
-            <Button variant="outlined" style={{marginTop: "20px"}} onClick={onSubmit}>Submit</Button>
-        </Container>
-    )
-}
-
-export default InputPage
+export default InputPage;
