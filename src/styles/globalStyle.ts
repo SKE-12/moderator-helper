7936adcbdd1dce4d75e12c9f91d9cf77a75e6843
gunphasin <phasin@wongnai.com>
2020-09-12 02:00:00 +0700

    fix voteoutmodal bug

---
+++ src/styles/globalStyle.ts
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    button:disabled {
+        background-color: #c0c0c0 !important;
+    }
+`;
+
+export default GlobalStyle;
