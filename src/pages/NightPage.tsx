192407250a3bf26b31f951ed4ca2c05fa8883bd3
gunphasin <phasin@wongnai.com>
2020-09-12 02:37:59 +0700

    show role image in NightPhase

--- src/pages/NightPage.tsx
+++ src/pages/NightPage.tsx
@@ -1,93 +1,129 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { without } from 'lodash';
+import { without } from "lodash";
 // @ts-ignore
-import Select from 'react-dropdown-select';
-import { useHistory } from 'react-router-dom';
-import {
-  Button,
-  Gap,
-} from 'solarxui';
-import styled from 'styled-components';
+import Select from "react-dropdown-select";
+import { useHistory } from "react-router-dom";
+import { Button, Gap, Photo } from "solarxui";
+import styled from "styled-components";
 
-import { useGameState } from '../contexts/gameController';
-import { RoleName } from '../models/Allegiance';
-import Player from '../models/Player';
-import ModResponseModal from '../components/ModResponseModal';
+import { useGameState } from "../contexts/gameController";
+import { RoleName } from "../models/Allegiance";
+import Player from "../models/Player";
+import ModResponseModal from "../components/ModResponseModal";
+import roleImgResolver, { RoleImageName } from "../roleImgResolver";
 
 const Container = styled.div`
-    text-align: center;
-`
+  text-align: center;
+`;
 
 const Night = () => {
-    const [currentSelect, setCurrentSelect] = useState<Player>()
-    const [secondarySelect, setSecondarySelect] = useState<Player>()
-    const { players, targetPlayers, nightAction, endNight, killTarget } = useGameState(gameState => ({
-        players: gameState.getRoleForNightCall(),
-        targetPlayers: gameState.getAlivePlayer().map(player => ({
-            value: player,
-            label: player.playerName,
-        })),
-        nightAction: gameState.nightAction,
-        endNight: gameState.endNight,
-        killTarget: gameState.killTarget,
-    }))
-    const [modResponse, setModResponse] = useState<boolean|null>(null)
-    const [toBeTakeActionPlayers, setToBeTakeActionPlayers] = useState<Player[]>(players)
-    const history = useHistory()
+  const [currentSelect, setCurrentSelect] = useState<Player>();
+  const [secondarySelect, setSecondarySelect] = useState<Player>();
+  const {
+    players,
+    targetPlayers,
+    nightAction,
+    endNight,
+    killTarget,
+  } = useGameState((gameState) => ({
+    players: gameState.getRoleForNightCall(),
+    targetPlayers: gameState.getAlivePlayer().map((player) => ({
+      value: player,
+      label: player.playerName,
+    })),
+    nightAction: gameState.nightAction,
+    endNight: gameState.endNight,
+    killTarget: gameState.killTarget,
+  }));
+  const [modResponse, setModResponse] = useState<boolean | null>(null);
+  const [toBeTakeActionPlayers, setToBeTakeActionPlayers] = useState<Player[]>(
+    players
+  );
+  const history = useHistory();
 
-    const onSubmit = () => {
-        const currentPlayer = toBeTakeActionPlayers[0]
-        const modResponse = nightAction(currentPlayer, [currentSelect!, secondarySelect!])
-        setModResponse(modResponse)
+  const onSubmit = () => {
+    const currentPlayer = toBeTakeActionPlayers[0];
+    const modResponse = nightAction(currentPlayer, [
+      currentSelect!,
+      secondarySelect!,
+    ]);
+    setModResponse(modResponse);
 
-        const updatedList = without(toBeTakeActionPlayers, currentPlayer)
-        if (updatedList.length === 0) {
-            endNight()
-            history.push('/summary')
-        } else {
-            setCurrentSelect(undefined)
-            setSecondarySelect(undefined)
-            setToBeTakeActionPlayers(updatedList)
-        }
+    const updatedList = without(toBeTakeActionPlayers, currentPlayer);
+    if (updatedList.length === 0) {
+      endNight();
+      history.push("/summary");
+    } else {
+      setCurrentSelect(undefined);
+      setSecondarySelect(undefined);
+      setToBeTakeActionPlayers(updatedList);
     }
+  };
 
-    const onSkip = () =>  {
-        const currentPlayer = toBeTakeActionPlayers[0]
+  const onSkip = () => {
+    const currentPlayer = toBeTakeActionPlayers[0];
 
-        const updatedList = without(toBeTakeActionPlayers, currentPlayer)
-        if (updatedList.length === 0) {
-            endNight()
-            history.push('/summary')
-        } else {
-            setCurrentSelect(undefined)
-            setSecondarySelect(undefined)
-            setToBeTakeActionPlayers(updatedList)
-        }
+    const updatedList = without(toBeTakeActionPlayers, currentPlayer);
+    if (updatedList.length === 0) {
+      endNight();
+      history.push("/summary");
+    } else {
+      setCurrentSelect(undefined);
+      setSecondarySelect(undefined);
+      setToBeTakeActionPlayers(updatedList);
     }
+  };
 
-    const { role, modDescription, isAlive } = toBeTakeActionPlayers[0]
+  const { role, modDescription, isAlive } = toBeTakeActionPlayers[0];
 
-    return (
-        <Gap type="vertical" size="8px">
-            <h1>Night Phase</h1>
-            <div style={{ textAlign: 'center' }}>
-                {/* <Photo size={300} src={roleImgResolver(role.toLowerCase().split(' ').join('_'))} /> */}
-            </div>
-            <Container>
-                <div className="bold">{role}</div>
-                <div>{role !== RoleName.FALLEN_ANGEL && `\`\` ${modDescription} \`\``}</div>
-                <div>{role === RoleName.FALLEN_ANGEL && `\`\` Fallen angel ลืมตา (ให้ชี้ไปที่คนในรายชื่อนี้ ${killTarget.map(player => player.playerName).join(',')}) กำลังจะตาย หากต้องการใช้พลังให้ชี้เป้าหมาย \`\``}</div>
-            </Container>
-            {/** @ts-ignore */}
-            <Select options={targetPlayers} values={currentSelect} onChange={([value]) => setCurrentSelect(value.value)} />
-            {/** @ts-ignore */}
-            <Select options={targetPlayers} values={secondarySelect} onChange={([value]) => setSecondarySelect(value.value)} disabled={role !== RoleName.FALLEN_ANGEL} />
-            <Button hidden={!isAlive} disabled={currentSelect === undefined} onClick={onSubmit}>Submit</Button>
-            <Button onClick={onSkip} hidden={role !== RoleName.FALLEN_ANGEL && isAlive}>Skip</Button>
-            <ModResponseModal modResponse={modResponse} onOk={setModResponse} />
-        </Gap>
-    )
-}
+  return (
+    <Gap type="vertical" size="8px">
+      <h1>Night Phase</h1>
+      <div style={{ textAlign: "center" }}>
+        <Photo size={300} src={roleImgResolver(role)} />
+      </div>
+      <Container>
+        <div className="bold">{role}</div>
+        <div>
+          {role !== RoleName.FALLEN_ANGEL && `\`\` ${modDescription} \`\``}
+        </div>
+        <div>
+          {role === RoleName.FALLEN_ANGEL &&
+            `\`\` Fallen angel ลืมตา (ให้ชี้ไปที่คนในรายชื่อนี้ ${killTarget
+              .map((player) => player.playerName)
+              .join(",")}) กำลังจะตาย หากต้องการใช้พลังให้ชี้เป้าหมาย \`\``}
+        </div>
+      </Container>
+      {/** @ts-ignore */}
+      <Select
+        options={targetPlayers}
+        values={currentSelect as any}
+        onChange={([value]) => setCurrentSelect(value.value)}
+      />
+      {/** @ts-ignore */}
+      <Select
+        options={targetPlayers}
+        values={secondarySelect as any}
+        onChange={([value]) => setSecondarySelect(value.value)}
+        disabled={role !== RoleName.FALLEN_ANGEL}
+      />
+      <Button
+        hidden={!isAlive}
+        disabled={currentSelect === undefined}
+        onClick={onSubmit}
+      >
+        Submit
+      </Button>
+      <Button
+        onClick={onSkip}
+        hidden={role !== RoleName.FALLEN_ANGEL && isAlive}
+      >
+        Skip
+      </Button>
+      <ModResponseModal modResponse={modResponse} onOk={setModResponse} />
+    </Gap>
+  );
+};
 
-export default Night
+export default Night;
